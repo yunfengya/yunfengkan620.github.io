53636c3ccf094989336090fcd60e56eaf604381e
DESKTOP-4FN26JQ\Administrator <Yunfeng.Kan@luxsan-ict.com>
2024-09-02 11:43:57 +0800

    111

--- vue.config.js
+++ vue.config.js
@@ -7,7 +7,10 @@ module.exports = {
   // transpileDependencies: true,
   lintOnSave: false,
   // publicPath: '/',
-  publicPath: '/yunfengkan620.github.io/',
+  // publicPath: '/yunfengkan620.github.io/',
+  // 默认情况下，Vue CLI 会假设你的应用是被部署在一个域名的根路径上
+  // 例如 https://www.ruoyi.vip/。如果应用被部署在一个子路径上，你就需要用这个选项指定这个子路径。例如，如果你的应用被部署在 https://www.ruoyi.vip/admin/，则设置 baseUrl 为 /admin/。
+  publicPath: process.env.NODE_ENV === "production" ? "/yunfengkan620.github.io/" : "/",
   outputDir: 'dist',
   assetsDir: 'static',
   productionSourceMap: false,
