e0010f3c1dd40539a49f97d61c82cc8de305bbce
DESKTOP-4FN26JQ\Administrator <Yunfeng.Kan@luxsan-ict.com>
2024-09-11 13:22:28 +0800

    1

--- src/pages/echart/echart001.vue
+++ src/pages/echart/echart001.vue
@@ -1,34 +1,102 @@
 <template>
-    <div class="contain_box">
-        <h1>echart001</h1>
+    <div class="statusBox" v-show="true">
+        <el-row class="elRow">
+            <el-scrollbar class="scrollbar_top">
+                <div v-for="(item, index) in nameList" :key="index" :class="activeIndex == index ? 'activeItem' : ''"
+                    class="itemDiv" @click="chooseItem(index, item.name)">
+                    {{ item.label }}
+                </div>
+            </el-scrollbar>
+        </el-row>
+        <keep-alive>
+            <component :is="activeName" :ref="activeName"></component>
+        </keep-alive>
     </div>
 </template>
 <script>
+import echartItem001 from "./echart001components/echartItem001";
+
 export default {
-    name: "index",
-    components: {},
+    name: "error404",
+    props: [""],
+    components: {
+        echartItem001,
+    },
     data() {
         return {
-            // 遮罩层
-            loading: true,
+            activeIndex: 0,
+            activeName: "echartItem001",
+            nameList: [
+                { label: "echartItem001", name: "echartItem001" },
+            ],
         };
     },
-    watch: {},
     created() { },
-    methods: {},
+    mounted() { },
+    methods: {
+        chooseItem(idnex, name) {
+            this.activeIndex = idnex;
+            this.activeName = name;
+        },
+    },
 };
 </script>
-  
-<style scoped lang="scss">
-.contain_box {
-    width: 100%;
-    height: 100%;
-    overflow: auto;
 
-    h1 {
-        font-size: 3rem;
-        color: #b4a4a4;
-        text-align: center;
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+.statusBox {
+    // padding: 1rem 1rem;
+    // height: calc(100vh - 2rem);
+    padding: 0rem;
+    height: calc(100vh - 0rem);
+    background-color: #e1e1e1;
+    // background-color: #1f3e6f;
+}
+
+.elRow {
+    height: 4.2rem;
+    width: 100%;
+    // display: flex;
+    ::v-deep .scrollbar_top {
+        width: 100%;
+        height: 100% !important;
+        .el-scrollbar__wrap{
+            overflow: hidden;
+            width: 100%;
+            height: 100% !important;
+            .el-scrollbar__view {
+                width: 100%;
+                height: 100% !important;
+                display: flex;
+            }
+        }
+        .el-scrollbar__thumb{
+            background-color: #bdf7bb;
+        }
     }
+}
+
+.itemDiv {
+    flex-shrink: 0;
+    width: 13rem;
+    height: 4.2rem;
+    text-align: center;
+    line-height: 4.2rem;
+    color: #36909e;
+    font-size: 1.6rem;
+    // float: left;
+    margin-right: 1.2rem;
+    background-color: #23477d;
+    // background: url("~@/assets/images/equipment/wxz.png") no-repeat;
+    // background-size: 100% 100%;
+    cursor: pointer;
+}
+
+.activeItem {
+    font-weight: bold;
+    color: #17e1ff;
+    background-color: #2b599e;
+    // background: url("~@/assets/images/equipment/xz.png") no-repeat;
+    // background-size: 100% 100%;
 }
 </style>
---
+++ src/pages/echart/echart001components/echartItem001.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="contain_box">
+        111
+    </div>
+</template>
+<script>
+export default {
+    name: "index",
+    components: {},
+    data() {
+        return {
+            // 遮罩层
+            loading: true,
+        };
+    },
+    watch: {},
+    created() { },
+    methods: {
+        
+    },
+};
+</script>
+  
+<style scoped lang="scss">
+.contain_box {
+    width: 100%;
+    // height: 100%;
+    height: calc(100% - 4.2rem);
+    overflow: auto;
+    // background: url("@/assets/bgc.jpg") no-repeat;
+    // background-size: 100% 100%;
+}
+</style>
