352fb92e515e5891ad4b631c6993dd7e14a806a4
DESKTOP-4FN26JQ\Administrator <Yunfeng.Kan@luxsan-ict.com>
2024-09-14 09:22:31 +0800

    1

---
+++ src/pages/cssGongNeng/css001components/demo.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="contain_box">
+        <div>fff</div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "index",
+    data() {
+        return {
+            
+        };
+    },
+    mounted() {
+        
+    },
+    beforeDestroy() {
+        
+    },
+    methods: {
+        
+    }
+};
+</script>
+
+<style scoped>
+/* 容器样式 */
+.contain_box {
+    width: 100%;
+    height: calc(100vh - 4.2rem);
+    /* 视口的高度减去顶部的导航栏高度 */
+    overflow: hidden;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border: 1px solid gray;
+    /* 外框样式 */
+}
+
+</style>
