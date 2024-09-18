899f1b7678720328723474a0e7b643444451fec2
DESKTOP-4FN26JQ\Administrator <Yunfeng.Kan@luxsan-ict.com>
2024-09-18 19:19:53 +0800

    11

--- src/pages/echart/echart001components/echartItem002.vue
+++ src/pages/echart/echart001components/echartItem002.vue
@@ -1,10 +1,7 @@
 <template>
     <div class="contain_box">
-        <div 
-            class="echartsOne" 
-            ref="echartsPie1"
-            style="width:90%;height:80%;border:1px solid gray;backgroundColor:#4e6ca3;"
-        ></div>
+        <div class="echartsOne" ref="echartsPie1"
+            style="width:90%;height:80%;border:1px solid gray;backgroundColor:#4e6ca3;"></div>
     </div>
 </template>
 <script>
@@ -26,11 +23,83 @@ export default {
         myChartPie1() {
 
             let dataList = [
-                // { value: 2154, name: '小名', sex: '男' },
-                // { value: 3854, name: '小李', sex: '女' }
+                // {
+                //     value: 1603,
+                //     name: '互联网/计算机',
+                //     itemStyle: {
+                //         color: '#4B96F3'
+                //     }
+                // },
+                // {
+                //     value: 1199,
+                //     name: '通信/电子',
+                //     itemStyle: {
+                //         color: '#10C6A6'
+                //     }
+                // },
             ]
-            let option={
 
+            let dataname = ['互联网/计算机', '通信/电子', '房地产/建筑', '专业服务/教育', '制药/医疗', '能源', '政府', '其他1', '其他2', '其他3', '其他4']
+            let datvalue = [1603, 1599, 1000, 950, 500, 260, 240, 130, 120, 110, 100]
+            let colorList = ['#4B96F3', '#10C6A6', '#F2AF4B', '#FF4D4F', '#AA7AF1'];
+            datvalue.forEach((item,index)=>{
+                dataList.push(
+                    {
+                        value: item,
+                        name: dataname[index],
+                        itemStyle: {
+                            color: colorList[index%5],
+                        } 
+                    }
+                )
+            })
+
+            // var data = []
+            // for (var i = 0; i < dataname.length; i++) {
+            //     data.push({
+            //         value: datvalue[i],
+            //         name: dataname[i],
+            //         itemStyle: {
+            //             color: colorList[i]
+            //         }
+            //     })
+            // }
+
+            let option = {
+                tooltip: {
+                    trigger: 'item',
+                    formatter: "{b}: {c}"
+                },
+                series: [{
+                    type: 'treemap',
+                    width: '80%',
+                    height: '80%',
+                    breadcrumb: {
+                        show: false
+                    },
+                    label: {
+                        normal: {
+                            show: true,
+                            textStyle: {
+                                color: '#fff',
+                                fontSize: 16,
+                            },
+                        }
+                    },
+                    itemStyle: {
+                        normal: {
+                            show: true,
+                            borderWidth: 1,
+                            borderColor: '#fff',
+                        },
+                        emphasis: {
+                            label: {
+                                show: true
+                            }
+                        }
+                    },
+                    data: dataList
+                }]
             }
 
             let newOption = {};
@@ -57,13 +126,12 @@ export default {
             if (myChartPie === undefined) {
                 myChartPie = this.$echarts.init(this.$refs.echartsPie1);
             }
-            
+
             // myChartPie.off("click");
             // myChartPie.on("click", (params) => {
             //     console.log(params);
             // });
 
-            myChartPie.setOption(newOption);
             const chartObserver = new ResizeObserver(() => {
                 myChartPie.setOption(newOption, true);
                 myChartPie.resize();
