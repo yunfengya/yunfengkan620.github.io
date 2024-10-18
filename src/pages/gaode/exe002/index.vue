7385684147fbff765b2e93be9a9aadd80f3652e5
DESKTOP-4FN26JQ\Administrator <Yunfeng.Kan@luxsan-ict.com>
2024-10-18 09:36:15 +0800

    1

--- src/pages/gaode/exe002/index.vue
+++ src/pages/gaode/exe002/index.vue
@@ -32,15 +32,14 @@ export default {
                 key: "d49126ab4c25960612aa4c0e60976654", // 申请好的Web端开发者Key，首次调用 load 时必填
                 version: "2.0", // 指定要加载的 JSAPI 的版本，缺省时默认为 1.4.15
                 plugins: ["AMap.Scale"], //需要使用的的插件列表，如比例尺'AMap.Scale'，支持添加多个如：['...','...']
+            }).then((AMap) => {
+                this.map = new AMap.Map("container", {
+                    // 设置地图容器id
+                    viewMode: "3D", // 是否为3D地图模式
+                    zoom: 11, // 初始化地图级别
+                    center: [116.397428, 39.90923], // 初始化地图中心点位置
+                });
             })
-                .then((AMap) => {
-                    this.map = new AMap.Map("container", {
-                        // 设置地图容器id
-                        viewMode: "3D", // 是否为3D地图模式
-                        zoom: 11, // 初始化地图级别
-                        center: [116.397428, 39.90923], // 初始化地图中心点位置
-                    });
-                })
                 .catch((e) => {
                     console.log(e);
                 });
