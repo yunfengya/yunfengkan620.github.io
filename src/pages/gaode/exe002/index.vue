94b1c2157fc31753b720c96330ed2f36e248a4e9
DESKTOP-4FN26JQ\Administrator <Yunfeng.Kan@luxsan-ict.com>
2024-10-18 09:21:04 +0800

    地图

--- src/pages/gaode/exe002/index.vue
+++ src/pages/gaode/exe002/index.vue
@@ -1,10 +1,11 @@
 <template>
     <div class="contain_box">
-        <h1>待开发2</h1>
+        <div id="container"></div>
     </div>
 </template>
 
 <script>
+import AMapLoader from '@amap/amap-jsapi-loader';
 export default {
     name: "index",
     data() {
@@ -14,13 +15,36 @@ export default {
         };
     },
     mounted() {
-        
+        this.initAMap();
+    },
+    unmounted() {
+        this.map?.destroy();
     },
     beforeDestroy() {
-        
+
     },
     methods: {
-        
+        initAMap() {
+            window._AMapSecurityConfig = {
+                securityJsCode: "16650193f5dbeb0d9c804c1cbcd081f4",
+            };
+            AMapLoader.load({
+                key: "d49126ab4c25960612aa4c0e60976654", // 申请好的Web端开发者Key，首次调用 load 时必填
+                version: "2.0", // 指定要加载的 JSAPI 的版本，缺省时默认为 1.4.15
+                plugins: ["AMap.Scale"], //需要使用的的插件列表，如比例尺'AMap.Scale'，支持添加多个如：['...','...']
+            })
+                .then((AMap) => {
+                    this.map = new AMap.Map("container", {
+                        // 设置地图容器id
+                        viewMode: "3D", // 是否为3D地图模式
+                        zoom: 11, // 初始化地图级别
+                        center: [116.397428, 39.90923], // 初始化地图中心点位置
+                    });
+                })
+                .catch((e) => {
+                    console.log(e);
+                });
+        },
     },
 };
 </script>
@@ -34,5 +58,10 @@ export default {
     display: flex;
     justify-content: center;
     align-items: center;
+
+    #container {
+        width: 100%;
+        height: 100%;
+    }
 }
 </style>
