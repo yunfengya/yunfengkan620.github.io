2250a23debad3ca7bc7318398304043319721a50
DESKTOP-4FN26JQ\Administrator <Yunfeng.Kan@luxsan-ict.com>
2024-10-29 14:05:37 +0800

    1

--- src/pages/cssGongNeng/css002components/cssItem001.vue
+++ src/pages/cssGongNeng/css002components/cssItem001.vue
@@ -18,7 +18,7 @@ export default {
     },
     data() {
         return {
-            selectedRange: [2,5],
+            selectedRange: [],
             options: [
                 { label: 'WK1', value: 1 },
                 { label: 'WK2', value: 2 },
