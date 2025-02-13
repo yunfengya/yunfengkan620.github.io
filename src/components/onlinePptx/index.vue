1870838832bfed879b6269b020af6541f17e0100
kanyunfeng <1500417027@qq.com>
2025-02-13 17:01:28 +0800

    1

---
+++ src/components/onlinePptx/index.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="contain_box">
+        <div>待开发</div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "index",
+    data() {
+        return {
+            
+        };
+    },
+    mounted() {
+        
+    },
+    beforeDestroy() {
+        
+    },
+    methods: {
+        
+    }
+};
+</script>
+
+<style scoped lang="scss">
+/* 容器样式 */
+.contain_box {
+    width: 100%;
+    height: calc(100vh - 4.2rem);
+    /* 视口的高度减去顶部的导航栏高度 */
+    overflow: hidden;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border: 1px solid gray;
+    /* 外框样式 */
+}
+
+</style>
