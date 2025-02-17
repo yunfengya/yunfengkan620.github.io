0317482be8bac0b416485c1d2c2ea0656a37f1ef
kanyunfeng <1500417027@qq.com>
2025-02-17 18:07:29 +0800

    1

--- src/pages/cssGongNeng/css002components/aaafirst.vue
+++ src/pages/cssGongNeng/css002components/aaafirst.vue
@@ -1,59 +1,24 @@
 <template>
     <div class="contain_box">
-        <el-button type="primary" @click="submitForm('')">查询接口</el-button>
-        <div>{{tableList}}</div>
+        <div>fff</div>
     </div>
 </template>
 
 <script>
-// 导入api.js中的callApi函数
-import { callApi } from '@/api/fatp_api.js';
 export default {
     name: "index",
     data() {
         return {
-            tableList:[],
+            
         };
     },
     mounted() {
-
+        
     },
     beforeDestroy() {
         
     },
     methods: {
-        submitForm(){
-            this.fetchData()
-        },
-
-        async fetchData() {
-            // 定义接口URL、参数和token
-            const endpoint = 'http://10.190.196.161:8301/peopleManagement/userActivity/activityRanking';
-            const params = {
-                date: '2025-02-17',
-                type: '0'
-                // ... 其他参数
-            };
-            // 你应该从某处获取这个token
-            const token = 'eyJhbGciOiJIUzUxMiJ9.eyJsb2dpbl91c2VyX2tleSI6ImNlMWQ4OWE0LWM2MjYtNDQ2Ni1iNDgwLTBkZTczYjcwYmQ3YSJ9.4SO5pINcyXLClGe00GfB7Gpv6FlP0pGs_WBFTsngo3ZsW7ftkFOXnJ-EImg64jDubmkCTlXy43hVA8iY4Je3vA'; 
-            const method = 'get'; // 或者'GET', 'PUT', 'DELETE'等
-            const timeout = 10000; // 10秒超时
-            const responseType = 'json'; // 或者'blob'
-            try {
-                // 调用封装好的接口函数
-                const data = await callApi(endpoint, params, token, method, timeout, responseType);
-                // 处理响应数据
-                console.log(data);
-                if(data.code==200){
-                    this.tableList = data.rows||[]
-                }else{
-                    this.$message.error(data.msg)
-                }
-            } catch (error) {
-                // 处理错误
-                console.error('There was a problem fetching the data:', error);
-            }
-        },
         
     }
 };
