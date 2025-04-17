3cc237c032a54f2bfcb02d10ddb8e52ba59de7d3
kanyunfeng <1500417027@qq.com>
2025-04-17 10:21:30 +0800

    1

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -112,8 +112,8 @@ export default {
           `,
         },
         {
-          name: "测试", status: "2",
-          desc: ``,
+          name: " 官方推荐 🔥 RuoYi-Vue 全新 Pro 版本，优化重构所有功能。基于 Spring Boot + MyBatis Plus + Vue & Element 实现的后台管理系统 + 微信小程序，支持 RBAC 动态权限、数据权限、SaaS 多租户、Flowable 工作流、三方登录、支付、短信、商城", status: "2",
+          desc: `若依新版：https://gitee.com/zhijiantianya/ruoyi-vue-pro`,
         },
       ],
     };
