9522f88be8c296b5b5d1252579bb7863efba0c31
kanyunfeng <1500417027@qq.com>
2025-04-18 11:17:13 +0800

    1

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -115,6 +115,10 @@ export default {
           name: " 官方推荐 🔥 RuoYi-Vue 全新 Pro 版本，优化重构所有功能。基于 Spring Boot + MyBatis Plus + Vue & Element 实现的后台管理系统 + 微信小程序，支持 RBAC 动态权限、数据权限、SaaS 多租户、Flowable 工作流、三方登录、支付、短信、商城", status: "2",
           desc: `若依新版：https://gitee.com/zhijiantianya/ruoyi-vue-pro`,
         },
+        {
+          name: "npm包流程节点图插件", status: "2",
+          desc: `npm包流程节点图插件：https://www.npmjs.com/package/@antv/x6/v/1.14.0`,
+        },
       ],
     };
   },
