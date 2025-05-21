e87b2eb248b46f1b9ca92a148217f6e5905eb7f6
kanyunfeng <1500417027@qq.com>
2025-05-21 15:10:05 +0800

    1

--- src/directive/drag.js
+++ src/directive/drag.js
@@ -3,6 +3,53 @@
 * Copyright (c) 2019 ruoyi
 */
 
+/* 
+使用方式
+<template>
+    <div class="contain_box">
+        <div v-dialogDrag class="dragDiv">自定义指令，可拖动元素</div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "index",
+    data() {
+        return {
+            
+        };
+    },
+    mounted() {
+        
+    },
+    beforeDestroy() {
+        
+    },
+    methods: {
+        
+    }
+};
+</script>
+
+<style scoped lang="scss">
+.contain_box {
+    width: 100%;
+    height: 100%;
+    // background-color: rgb(177, 175, 175);
+}
+.dragDiv {
+    background-color: #b0adad;
+    width: 120px;
+    height: 120px;
+    cursor: move;
+    z-index: 999999;
+    position: absolute;
+    bottom: 4rem;
+    right: 6rem;
+}
+
+</style> */
+
 export default {
   bind(el, binding, vnode, oldVnode) {
     // let oDiv = el.querySelector('.voiceIcon');
