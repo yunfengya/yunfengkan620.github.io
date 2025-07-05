ffce02f659e7caa398ceb43577259260e0751321
kanyunfeng <1500417027@qq.com>
2025-07-05 08:44:34 +0800

    1

--- src/pages/cssGongNeng/css002components/aaafirst.vue
+++ src/pages/cssGongNeng/css002components/aaafirst.vue
@@ -1,6 +1,15 @@
 <template>
     <div class="contain_box">
-        <div>fff</div>
+        <div>
+            控制台只需一行代码，任意网页秒变可编辑！
+            <br>
+            打开 document.designMode = "on";
+            <br>
+            关闭 document.designMode = "off"
+            <br>
+            Iframe嵌套页面的编辑：iframeNode.contentDocument.designMode = "on";
+
+        </div>
     </div>
 </template>
 
